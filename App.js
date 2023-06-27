8277847c98ac3f9c2358c6795b01dc202de58fac
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-06-27 17:29:28 -0600

    Modificacion y llamado a la vista Clima

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View} from 'react-native';
+import ClimaScreen from './src/ClimaScreen';
+
+const App = () => {
+  return (
+    <View>
+      <ClimaScreen />
+    </View>
+  );
+};
+
+export default App;
